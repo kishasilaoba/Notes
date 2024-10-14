6567a9867f101927544b79818e7169a35b8b10b9
Kimberly <mnkim.chan@gmail.com>
2024-10-14 23:01:08 +0800

    Auto commit on Mon Oct 14 23:01:08 MPST 2024

---
+++ .obsidian/auto-push.sh
@@ -0,0 +1,4 @@
+ 
+git add .
+git commit -m "Auto-commit on $(date)"
+git push origin master  
---
+++ auto-push.sh
@@ -0,0 +1,5 @@
+cd /c/Users/mnkim/Documents/Obsidian\ Vault
+git add .
+git commit -m "Auto commit on $(date)"
+git push origin master
+
